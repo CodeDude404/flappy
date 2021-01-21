ccd27beccfe2e8a0130a46da858a16f58c12ce55
CodeDude404 <68038925+CodeDude404@users.noreply.github.com>
2021-01-20 16:13:16 -0800

    COlor Chane Settings

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const header = document.getElementsByTagName("h1")[0];
-const button = document.getElementById("btn");
+const header1 = document.getElementsByTagName("h1")[2];
+const button = document.createElementById("btn");
 
 
 function changeColor() {
-	header.style.color = "aqua";
+	header1.style.color = "blue"
 }
 
+button.addEventListener("click", changeColor)
 
-button.addEventListener("click", changeColor);
+
+
+
 
