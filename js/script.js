3b4843cb20d7f6f9d20f22737ee205f907aad4e8
CodeDude404 <68038925+CodeDude404@users.noreply.github.com>
2021-02-03 15:30:54 -0800

    Set up varibles

--- js/script.js
+++ js/script.js
@@ -2,5 +2,10 @@
 var PLAYER_SIZE = 20;
 var CANVAS = document.getElementById("mycanvas");
 var SCORE_TEXT = document.getElementById("score");
-
+var CTX = CANVAS.getContext("2d");
+var GROUND_LEVEL = CANVAS.height - 50;
+var SCORE = 0;
+var OBSTICILES;
+var SCROLL_SPEED;
+var player;
 //Do some stuff here
