ff4e5b6afa4b820c166178ccf198ee21cc0cc92c
Liam Outlaw <liam.outlaw@gmail.com>
2021-02-17 22:57:57 +0000

    Remove note on line 6

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 var PLAYER_SIZE = 20;
 var CANVAS = document.getElementById("myCanvas");
 var SCORE_TEXT = document.getElementById("score");
-var CTX = CANVAS.getContext("2d");		//KNOWN BUG
+var CTX = CANVAS.getContext("2d");
 var GROUND_LEVEL = CANVAS.height - 50;
 var SCORE = 0;
 var OBSTICILES;
